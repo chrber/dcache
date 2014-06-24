fce7486878643868c4e116890a619f45457172b7
Christian Bernardt <christian.bernardt@desy.de>
2014-06-24 13:30:20 +0200

    Add /remote.php/webdav to OwnCloudHandler

--- modules/dcache-webdav/src/main/java/org/dcache/webdav/OwnCloudHandler.java
+++ modules/dcache-webdav/src/main/java/org/dcache/webdav/OwnCloudHandler.java
@@ -1,6 +1,5 @@
 package org.dcache.webdav;
 
-import io.milton.http.Auth;
 import io.milton.http.exceptions.NotAuthorizedException;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -31,14 +30,18 @@ public class OwnCloudHandler extends AbstractHandler {
         if (request.getContextPath().contains(OC_STATUS)) {
             OwnCloudResource ownCloudResource =  new OwnCloudResource();
             try {
-                ownCloudResource.authorise((io.milton.http.Request) baseRequest, io.milton.http.Request.Method.CONNECT, new Auth("", new String("")) );
+//                ownCloudResource.authorise(request, io.milton.http.Request.Method.CONNECT, new Auth("", new String("")) );
                 ownCloudResource.sendContent(response.getOutputStream(), null, baseRequest.getParameterMap(), baseRequest.getContentType());
             } catch (NotAuthorizedException e) {
-                _log.error("User not authorized to get ");
+                _log.error("User not authorized to get {}", request.getContextPath());
             }
             baseRequest.setHandled(true);
             response.getOutputStream().flush();
             response.flushBuffer();
+        }
+        if (request.getContextPath().contains(OC_PREFIX)) {
+            DcacheResourceFactory dcacheResourceFactory = new DcacheResourceFactory();
+            dcacheResourceFactory.getResource(baseRequest.getRemoteHost(), baseRequest.getContextPath());
         }
     }
 }
